12304a60b8d56b3bb54e26d19a971ef0b14be9cd
ricputra <rickyputra44@yahoo.com>
2014-11-17 22:05:12 +0700

    ni too
    inii tinggal lu taro gambar FireShoes,RemoveBoots,WaterFloor di folder
    gambar.a... nama file gambar.a yg ga tulis tadi..

--- Chip's Challenge/src/gui/Map.java
+++ Chip's Challenge/src/gui/Map.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gui;
 
 import java.awt.Color;
@@ -19,49 +18,144 @@ import javax.swing.JPanel;
  *
  * @author i13026 i13011
  */
-public class Map extends JPanel implements Runnable{
-    
-    BarrierGrid bg ;
-    Grid grid ;
+public class Map extends JPanel implements Runnable {
+
+    BarrierGrid bg;
+    Grid grid;
     FinishFloor finf;
     FireFloor fire;
     WallImage wall;
     ICImage ic;
     ChipDown chip;
-    
-    public Map(){
+    WaterBoots WB;
+    WaterFloor Water;
+    FireBoots FB;
+    RemoveBoots RB;
+
+    public Map() {
         Thread thread = new Thread(this);
         thread.start();
     }
 
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.gray);
-        for(int i=0;i<550;i+=50){
-            for(int j=0;j<550;j+=50){
-                grid = new Grid(i,j);
+        for (int i = 0; i < 550; i += 50) {
+            for (int j = 0; j < 550; j += 50) {
+                grid = new Grid(i, j);
                 grid.paintComponent(g2d);
-                if(i==250&&j==250){
-                    chip = new ChipDown(i,j);
-                    chip.paintComponent(g2d);
-                } else if(i==300&&j==250){
-                    fire = new FireFloor(i,j);
-                    fire.paintComponent(g2d);
-                }
+
             }
         }
-        bg = new BarrierGrid(50*8,50*8);
+        int s = 50;
+        chip = new ChipDown(5 * s, 1 * s);
+        chip.paintComponent(g2d);
+        ic = new ICImage(2 * s, 1 * s);
+        ic.paintComponent(g2d);
+        wall = new WallImage(3 * s, 1 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(2 * s, 2 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(3 * s, 2 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(2 * s, 3 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(3 * s, 3 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(3 * s, 4 * s);
+        wall.paintComponent(g2d);
+
+        wall = new WallImage(3 * s, 7 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(2 * s, 7 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(3 * s, 8 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(3 * s, 9 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(4 * s, 7 * s);
+        wall.paintComponent(g2d);
+
+        wall = new WallImage(6 * s, 7 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 7 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 8 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 9 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(8 * s, 7 * s);
+        wall.paintComponent(g2d);
+
+        wall = new WallImage(7 * s, 1 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 2 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(8 * s, 2 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(8 * s, 3 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 3 * s);
+        wall.paintComponent(g2d);
+        wall = new WallImage(7 * s, 4 * s);
+        wall.paintComponent(g2d);
+
+        bg = new BarrierGrid(5 * s, 7 * s);
         bg.paintComponent(g2d);
+
+        fire = new FireFloor(5 * s, 5 * s);
+        fire.paintComponent(g2d);
+        fire = new FireFloor(4 * s, 5 * s);
+        fire.paintComponent(g2d);
+        fire = new FireFloor(5 * s, 6 * s);
+        fire.paintComponent(g2d);
+        fire = new FireFloor(6 * s, 5 * s);
+        fire.paintComponent(g2d);
+        fire = new FireFloor(5 * s, 4 * s);
+        fire.paintComponent(g2d);
+
+        ic = new ICImage(2 * s, 1 * s);
+        ic.paintComponent(g2d);
+        ic = new ICImage(8 * s, 1 * s);
+        ic.paintComponent(g2d);
+        ic = new ICImage(2 * s, 9 * s);
+        ic.paintComponent(g2d);
+        ic = new ICImage(8 * s, 9 * s);
+        ic.paintComponent(g2d);
+
+        finf = new FinishFloor(5 * s, 9 * s);
+        finf.paintComponent(g2d);
+
+        WB = new WaterBoots(1 * s, 1 * s);
+        WB.paintComponent(g2d);
+        WB = new WaterBoots(9 * s, 1 * s);
+        WB.paintComponent(g2d);
+        WB = new WaterBoots(6 * s, 2 * s);
+        WB.paintComponent(g2d);
+
+        FB = new FireBoots(2 * s, 4 * s);
+        FB.paintComponent(g2d);
+        FB = new FireBoots(8 * s, 4 * s);
+        FB.paintComponent(g2d);
+        FB = new FireBoots(4 * s, 2 * s);
+        FB.paintComponent(g2d);
+
+        RB = new RemoveBoots(2 * s, 6 * s);
+        RB.paintComponent(g2d);
+        RB = new RemoveBoots(8 * s, 6 * s);
+        RB.paintComponent(g2d);
+
     }
-    
+
     @Override
     public void run() {
-        while(true){
+        while (true) {
             repaint();
-            try{
+            try {
                 Thread.sleep(100);
-            } catch(InterruptedException ex){}
+            } catch (InterruptedException ex) {
+            }
         }
     }
 }
---
+++ Chip's Challenge/src/gui/WaterBoots.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author RICKY
+ */
+public class WaterBoots extends JPanel {
+
+    Image image;
+    int x;
+    int y;
+
+    public WaterBoots(int x, int y) {
+        String imageName = "waterShoes.png";
+        ImageIcon icon = null;
+        this.x = x;
+        this.y = y;
+        java.net.URL imgURL = getClass().getClassLoader().getResource(imageName);
+        icon = new ImageIcon(imgURL);
+        image = icon.getImage();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.white);
+        g.drawImage(image, x, y, this);
+    }
+
+}
