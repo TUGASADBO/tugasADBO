43d5f44888ba3b08099c8ee801b8df0c008b6521
ricputra <rickyputra44@yahoo.com>
2014-11-21 13:55:39 +0700

    map level2

--- Chip's Challenge/src/engine/BoardLevel2.java
+++ Chip's Challenge/src/engine/BoardLevel2.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package engine;
 
 /**
@@ -11,8 +10,9 @@ package engine;
  * @author i13026 i13011
  */
 public class BoardLevel2 {
+
     private Floor[][] floor;
-    
+
     protected boolean isFinish;
     protected int chipLeft;
     protected boolean isGameOver;
@@ -29,84 +29,93 @@ public class BoardLevel2 {
                 floor[10][i] = new Wall();
             }
         }
-        floor[1][1] = new PlainFloor();
+        floor[1][1] = new FireFloor();
         floor[2][1] = new PlainFloor();
         floor[3][1] = new PlainFloor();
-        floor[4][1] = new IntegratedCircuit();chipLeft++;
-        floor[5][1] = new Wall();
-        floor[6][1] = new IntegratedCircuit();chipLeft++;
-        floor[7][1] = new PlainFloor();
-        floor[8][1] = new PlainFloor();
-        floor[9][1] = new PlainFloor();
+        floor[4][1] = new PlainFloor();
+        floor[5][1] = new FireFloor();
+        floor[6][1] = new Wall();
+        floor[7][1] = new IntegratedCircuit();
+        chipLeft++;
+        floor[8][1] = new FireFloor();
+        floor[9][1] = new FireFloor();
 
         floor[1][2] = new PlainFloor();
-        floor[2][2] = new FireFloor();
+        floor[2][2] = new PlainFloor();
         floor[3][2] = new FireFloor();
-        floor[4][2] = new FireFloor();
-        floor[5][2] = new Wall();
-        floor[6][2] = new FireFloor();
-        floor[7][2] = new FireFloor();
-        floor[8][2] = new FireFloor();
-        floor[9][2] = new PlainFloor();
+        floor[4][2] = new PlainFloor();
+        floor[5][2] = new FireFloor();
+        floor[6][2] = new Wall();
+        floor[7][2] = new Wall();
+        floor[8][2] = new Wall();
+        floor[9][2] = new FireFloor();
 
         floor[1][3] = new PlainFloor();
-        floor[2][3] = new PlainFloor();
-        floor[3][3] = new PlainFloor();
-        floor[4][3] = new PlainFloor();
-        floor[5][3] = new Wall();
-        floor[6][3] = new PlainFloor();
-        floor[7][3] = new PlainFloor();
-        floor[8][3] = new PlainFloor();
-        floor[9][3] = new PlainFloor();
-        for (int i = 1; i < floor.length - 1; i++) {
-            floor[i][4] = new PlainFloor();
+        floor[2][3] = new Wall();
+        floor[3][3] = new Wall();
+
+        for (int i = 4; i < floor.length - 1; i++) {
+            floor[i][3] = new PlainFloor();
         }
-        
+        floor[1][4] = new PlainFloor();
+        floor[2][4] = new FireBoots();
+        floor[3][4] = new Wall();
+        floor[4][4] = new Wall();
+        floor[5][4] = new Wall();
+        floor[6][4] = new Wall();
+        floor[7][4] = new PlainFloor();
+        floor[8][4] = new Wall();
+        floor[9][4] = new PlainFloor();
+
         floor[1][5] = new FireFloor();
-        floor[2][5] = new FireFloor();
-        floor[3][5] = new PlainFloor();
+        floor[2][5] = new IntegratedCircuit();
+        chipLeft++;
+        floor[3][5] = new Wall();
         floor[4][5] = new PlainFloor();
         floor[5][5] = new PlainFloor();
         floor[6][5] = new PlainFloor();
         floor[7][5] = new PlainFloor();
-        floor[8][5] = new FireFloor();
+        floor[8][5] = new Wall();
         floor[9][5] = new FireFloor();
-        
-        for (int i = 2; i < floor.length - 2; i++) {
-            floor[i][6] = new PlainFloor();
+
+        floor[1][6] = new Wall();
+        floor[2][6] = new Wall();
+        floor[3][6] = new Wall();
+        floor[4][6] = new PlainFloor();
+        floor[5][6] = new PlainFloor();
+        floor[6][6] = new PlainFloor();
+        floor[7][6] = new Wall();
+        floor[8][6] = new Wall();
+        floor[9][6] = new IntegratedCircuit();
+        chipLeft++;
+
+        for (int i = 1; i < 6; i++) {
+            floor[i][7] = new PlainFloor();
         }
-        floor[1][6] = new FireFloor();
-        floor[9][6] = new FireFloor();
-        
-        floor[1][7] = new FireFloor();
-        floor[2][7] = new PlainFloor();
-        floor[3][7] = new Wall();
-        floor[4][7] = new Wall();
-        floor[5][7] = new Barrier(); 
-        floor[6][7] = new Wall();
         floor[7][7] = new Wall();
-        floor[8][7] = new PlainFloor();
-        floor[9][7] = new FireFloor();
-        
+        floor[8][7] = new FireFloor();
+        floor[9][7] = new IntegratedCircuit();
+        chipLeft++;
+
         floor[1][8] = new FireFloor();
-        floor[2][8] = new PlainFloor();
+        floor[2][8] = new Wall();
         floor[3][8] = new Wall();
-        floor[4][8] = new PlainFloor();
-        floor[5][8] = new PlainFloor();
-        floor[6][8] = new PlainFloor();
-        floor[7][8] = new Wall();
-        floor[8][8] = new PlainFloor();
-        floor[9][8] = new FireFloor();
-        
-        floor[1][9] = new IntegratedCircuit();chipLeft++;
-        floor[2][9] = new PlainFloor();
-        floor[3][9] = new Wall();
-        floor[4][9] = new PlainFloor();
-        floor[5][9] = new FinishFloor();
-        floor[6][9] = new PlainFloor();
-        floor[7][9] = new Wall();
-        floor[8][9] = new PlainFloor();
-        floor[9][9] = new IntegratedCircuit();chipLeft++;
+        floor[4][8] = new Wall();
+        floor[5][8] = new Barrier();
+        for (int i = 6; i < floor.length - 1; i++) {
+            floor[i][8] = new Wall();
+        }
+
+        floor[1][9] = new FireFloor();
+        floor[2][9] = new FireFloor();
+        floor[3][9] = new IntegratedCircuit();
+        chipLeft++;
+        floor[4][9] = new Wall();
+        floor[5][9] = new PlainFloor();
+        floor[6][9] = new FireFloor();
+        floor[7][9] = new FireFloor();
+        floor[8][9] = new FireFloor();
+        floor[9][9] = new FinishFloor();
     }
 
     public void setFloor(Floor flo, int x, int y) {
@@ -120,28 +129,28 @@ public class BoardLevel2 {
     public int getLength() {
         return this.floor.length;
     }
-        
-    public int getChipLeft(){
+
+    public int getChipLeft() {
         return this.chipLeft;
     }
-    
-    public void setChipLeft(){
+
+    public void setChipLeft() {
         this.chipLeft--;
     }
-    
-    public boolean getIsFinish(){
+
+    public boolean getIsFinish() {
         return isFinish;
     }
-    
-    public void setFinish(){
-        isFinish=true;
+
+    public void setFinish() {
+        isFinish = true;
     }
-    
-    public boolean getIsGameOver(){
+
+    public boolean getIsGameOver() {
         return isGameOver;
     }
-    
-    public void setIsGameOver(){
-        isGameOver=true;
+
+    public void setIsGameOver() {
+        isGameOver = true;
     }
 }
